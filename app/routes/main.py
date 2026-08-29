8194706e7bd0fc825482d3912bcf0d4b22339dad
lucas <lucasverissimo1799@gmail.com>
2026-08-29 12:33:00 -0300

    Adiciona rotas de produtos CRUD e upload para arquivo

--- app/routes/main.py
+++ app/routes/main.py
@@ -6,10 +6,37 @@ main_bp = Blueprint('main_bp', __name__)
 def index():
     return jsonify({"message": "Bem vindo"})
 
+# GET PRODUTOS
 @main_bp.route('/products')
 def get_products():
     return jsonify({"message": "Todos os Produtos"})
 
+# POST PRODUTOS
+@main_bp.route('/products', methods=['POST'])
+def create_product():
+    return jsonify({"message": "Postar Produtos"})
+
+# GET UM PRODUTO
+@main_bp.route('/products/<int:product_id>')
+def get_product_by_id(product_id):
+    return jsonify({"message": f"Um unico produto {product_id}"})
+
+# ATUALIZA UM PRODUTO
+@main_bp.route('/products/<int:product_id>', methods=['PUT'])
+def update_product(product_id):
+    return jsonify({"message": f"Atualiza {product_id}"})
+
+# DELETE UM PRODUTO
+@main_bp.route('/products/<int:product_id>', methods=['PUT'])
+def delete_product(product_id):
+    return jsonify({"message": f"Delete {product_id}"})
+
+# IMPORTA PARA UM ARQUIVO
+@main_bp.route('/sales/upload', methods=['POST'])
+def upload_dales():
+    return jsonify({"message": "Upload arquivos"})
+
 @main_bp.route('/login', methods=['POST'])
 def login():
-    return jsonify({"message": "Todos os Produtos"})
+    return jsonify({"message": "Realizar Login"})
+
